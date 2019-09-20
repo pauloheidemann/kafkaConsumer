972bfe51156ac7c564c5bcf7b70a417faa023d84
Paulo Henrique Heidemann <e016486@celesc.corp>
2019-09-20 17:49:43 -0300

    adição de dockerfile e implementação de subscriber do topico de teste do kafka

--- src/main/java/br/com/celesc/kafkaConsumerIntegradorGenesisKaffa/consumer/KafkaConstants.java
+++ src/main/java/br/com/celesc/kafkaConsumerIntegradorGenesisKaffa/consumer/KafkaConstants.java
@@ -8,14 +8,15 @@ package br.com.celesc.kafkaConsumerIntegradorGenesisKaffa.consumer;
  */
 public interface KafkaConstants {
 
-    public static String KAFKA_BROKERS = "localhost:9092";
+    public static String KAFKA_BROKERS = "celkafkah.celesc.com.br:9092";
     public static Integer MESSAGE_COUNT=1000;
     public static String CLIENT_ID="client1";
-    public static String TOPIC_NAME="genesis-kaffa";
+    public static String TOPIC_NAME="genesis-kaffa-integracao-TESTE_KAFKA";
     public static String GROUP_ID_CONFIG="genesis-kaffa-01";
     public static Integer MAX_NO_MESSAGE_FOUND_COUNT=5;
     public static String OFFSET_RESET_LATEST="latest";
     public static String OFFSET_RESET_EARLIER="earliest";
-    public static Integer MAX_POLL_RECORDS=1;
+    public static Integer MAX_POLL_RECORDS=100;
+    public static Integer MAX_POLL_INTERVAL_MS=300000;
 	
 }
